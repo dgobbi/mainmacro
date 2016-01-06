32969fd7b099786c58997c3adc3b982519785b5c
David Gobbi <david.gobbi@gmail.com>
2016-01-05 21:04:31 -0700

    Add 2016 to the copyright date list.

--- mainmacro.cpp
+++ mainmacro.cpp
@@ -1,8 +1,6 @@
 /*=========================================================================
 
-  Program: mainmacro
-
-  Copyright (c) 2015 David Gobbi
+  Copyright (c) 2015,2016 David Gobbi
   All rights reserved.
 
   Redistribution and use in source and binary forms, with or without
